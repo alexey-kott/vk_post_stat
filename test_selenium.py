dc26d1ea053da4e78801681a49463ba1ef0d737d
Alexey Kott <alexey.kott@gmail.com>
2018-09-09 03:25:03 +0300

    Add --no-sandbox option

--- test_selenium.py
+++ test_selenium.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     options = ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     options.add_argument("--window-size=%s" % WINDOW_SIZE)
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
