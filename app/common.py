11145d82beda7b7542268111d9b8d302ca6aff13
Alexey Kott <alexey.kott@gmail.com>
2018-09-09 04:03:38 +0300

    Fix websocket protocol connection

--- app/common.py
+++ app/common.py
@@ -214,7 +214,6 @@ def take_screenshot(link: str):
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--no-sandbox")
-    # options.experimental_options("useAutomationExtension", False)
     driver = Chrome("./webdriver/chromedriver", chrome_options=options)
 
     driver.get(link)
